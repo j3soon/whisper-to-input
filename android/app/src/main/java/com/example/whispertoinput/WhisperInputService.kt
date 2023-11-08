18df1d4ca8c650367e189383a2c83dae226b0366
tigerpaws <boshow2270@gmail.com>
2023-11-08 15:16:34 +0800

    feat(.WhisperInputService): Added implementation interfaces and member variables.
    Implementation interfaces either come from OnKeyboardActionListener or InputMethodService.
    Reference for member variables declaration: https://www.androidauthority.com/lets-build-custom-keyboard-android-832362/

--- android/app/src/main/java/com/example/whispertoinput/WhisperInputService.kt
+++ android/app/src/main/java/com/example/whispertoinput/WhisperInputService.kt
@@ -1,7 +1,49 @@
 package com.example.whispertoinput
 
 import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
+import android.view.KeyboardShortcutInfo
+import android.view.View
 
 class WhisperInputService : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+    private var keyboardView : KeyboardView? = null
+    private var keyboard : Keyboard? = null
+    private var caps = false
+
+    override fun onPress(primaryCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRelease(primaryCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onText(text: CharSequence?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeLeft() {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeRight() {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeDown() {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateInputView(): View {
+        return super.onCreateInputView()
+    }
 }
