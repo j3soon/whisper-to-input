73322efda79d8c5cb2f9fdca3608a7031eb706d0
tigerpaws <boshow2270@gmail.com>
2023-11-12 15:17:33 +0800

    feat(.WhisperInputService): Set up the keyboard view. Assigned a onClick event to button.
    Ideally, UI handling will be refactored to be another class in the future.

--- android/app/src/main/java/com/example/whispertoinput/WhisperInputService.kt
+++ android/app/src/main/java/com/example/whispertoinput/WhisperInputService.kt
@@ -2,16 +2,32 @@ package com.example.whispertoinput
 
 import android.inputmethodservice.InputMethodService
 import android.text.TextUtils
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
+import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
 
 
-class WhisperInputService : InputMethodService() {
+class WhisperInputService : InputMethodService()
+{
     private var keyboardView : ConstraintLayout? = null
 
-    override fun onCreateInputView(): View {
+    private fun setupKeyboardView()
+    {
+        val buttonMic = keyboardView!!.getViewById(R.id.btn_mic) as ImageButton
+        buttonMic.setOnClickListener{ onButtonMicClick(it) }
+    }
+
+    private fun onButtonMicClick(it: View)
+    {
+        val buttonMic = it as ImageButton
+    }
+
+    override fun onCreateInputView(): View
+    {
         keyboardView = layoutInflater.inflate(R.layout.keyboard_view, null) as ConstraintLayout
+        setupKeyboardView()
         return keyboardView!!
     }
 }
