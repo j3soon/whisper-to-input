38b88c5c2a94b919d886c3fa0d193ca238361916
tigerpaws <boshow2270@gmail.com>
2023-11-13 19:54:57 +0800

    refactor(.WhisperJobManager): Created the `WhisperJobManager` class.
    This class is responsible for managing ongoing transcription jobs.

---
+++ android/app/src/main/java/com/example/whispertoinput/WhisperJobManager.kt
@@ -0,0 +1,21 @@
+package com.example.whispertoinput
+
+import kotlinx.coroutines.*
+
+class WhisperJobManager
+{
+    fun startTranscriptionJobAsync(callback: (String?) -> Unit)
+    {
+        // TODO: Start a transcription job
+    }
+
+    fun clearTranscriptionJob()
+    {
+        // TODO: clear current transcription job
+    }
+
+    private fun registerTranscriptionJob(job : Job?)
+    {
+        // TODO: register a transcription job
+    }
+}
