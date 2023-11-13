5b732363bbdb82769f5329bdfd0cf72c05507482
tigerpaws <boshow2270@gmail.com>
2023-11-13 19:58:50 +0800

    refactor(.WhisperJobManager): Implemented `WhisperJobManager.startTranscriptionJobAsync`.
    Its code is almost the same as in `WhisperInputService` when calling `transcribeAsync`, but refactored to be in `WhisperJobManager`.

--- android/app/src/main/java/com/example/whispertoinput/WhisperJobManager.kt
+++ android/app/src/main/java/com/example/whispertoinput/WhisperJobManager.kt
@@ -8,7 +8,36 @@ class WhisperJobManager
 
     fun startTranscriptionJobAsync(callback: (String?) -> Unit)
     {
-        // TODO: Start a transcription job
+        suspend fun whisperTranscription(): String {
+            // TODO: Make Whisper requests to transcribe
+            // For now a text is returned after some predetermined time.
+            delay(3000)
+            return "Text 文字文字"
+        }
+
+        // Create a cancellable job in the main thread (for UI updating)
+        val job = CoroutineScope(Dispatchers.Main).launch {
+
+            // Within the job, make a suspend call at the I/O thread
+            // It suspends before result is obtained.
+            val result = withContext(Dispatchers.IO) {
+                try {
+                    // Perform transcription here
+                    return@withContext whisperTranscription()
+                } catch (e: CancellationException) {
+                    // Task was canceled
+                    return@withContext null
+                }
+            }
+
+            // This callback is within the main thread.
+            if (!result.isNullOrEmpty())
+            {
+                callback.invoke(result)
+            }
+        }
+
+        registerTranscriptionJob(job)
     }
 
     fun clearTranscriptionJob()
