09f9abcb0fadbff2c7be19ab76a810de37256c87
tigerpaws <boshow2270@gmail.com>
2023-11-15 17:53:00 +0800

    feat(.MainActivity): Requests microphone permission upon `onCreate`.
    Requests permission via `ActivityCompat.requestPermissions`.
    A request code is given to distinguish between requests. It has no other meaning.
    An `onRequestPermissionsResult` is overriden to process request results. In this case, if the permission is not given, a toast message shows up.
    
    Refs:
    - https://developer.android.com/guide/topics/media/platform/mediarecorder
    - https://developer.android.com/training/permissions/requesting

--- android/app/src/main/java/com/example/whispertoinput/MainActivity.kt
+++ android/app/src/main/java/com/example/whispertoinput/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.whispertoinput
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+
+private const val MICROPHONE_PERMISSION_REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity() {
+    private val permissions : Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(this, permissions, MICROPHONE_PERMISSION_REQUEST_CODE)
         setContentView(R.layout.activity_main)
     }
+
+    // Handles the results of permission requests.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // Only handles requests marked with the unique code.
+        if (requestCode != MICROPHONE_PERMISSION_REQUEST_CODE)
+        {
+            return
+        }
+
+        // All permissions should be granted.
+        for (result in grantResults)
+        {
+            if (result != PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, getString(R.string.mic_permission_required), Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+    }
+
 }
