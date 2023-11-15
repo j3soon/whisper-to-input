ae06eb98664117e97c2e989edb504124ff4009c7
tigerpaws <boshow2270@gmail.com>
2023-11-15 21:45:38 +0800

    feat(build.gradle): Added dependencies for settings up OpenAI Kotlin API.
    Followed the setup in the (un?)official OpenAI API for Kotlin: https://github.com/aallam/openai-kotlin/tree/main
    - `mavenCentral()` is omitted.
      - It's included in settings.gradle. Also refer to the following link, stating a change in the Gradle standards.
      - https://stackoverflow.com/questions/69163511/build-was-configured-to-prefer-settings-repositories-over-project-repositories-b
    - Setting up a Ktor engine: OkHttp is chosen due to information here
      - https://ktor.io/docs/http-client-engines.html
      - Version is from the latest entry found in here: https://mvnrepository.com/artifact/io.ktor/ktor-client-okhttp (under the Central tag)
      - Without setting up a client engine, exceptions will be thrown. See: https://github.com/ktorio/ktor/issues/1070

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.aallam.openai:openai-client:3.5.1")
+    implementation("io.ktor:ktor-client-okhttp:2.3.6")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
