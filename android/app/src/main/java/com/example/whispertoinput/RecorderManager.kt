baf5b85be59dd0ef1c6eed981bba61d2ea67d4bf
tigerpaws <boshow2270@gmail.com>
2023-11-16 16:55:11 +0800

    refactor(.RecorderManager): Implemented `start`, `stop`, and `setup`.
    The code is almost the same as in `WhisperInputService`.

--- android/app/src/main/java/com/example/whispertoinput/RecorderManager.kt
+++ android/app/src/main/java/com/example/whispertoinput/RecorderManager.kt
@@ -1,7 +1,13 @@
 package com.example.whispertoinput
 
 import android.Manifest
+import android.content.Context
 import android.media.MediaRecorder
+import android.os.Build
+import android.util.Log
+import java.io.IOException
+
+private const val MEDIA_RECORDER_CONSTRUCTOR_DEPRECATION_API_LEVEL = 31
 
 class RecorderManager {
     companion object {
@@ -9,13 +15,47 @@ class RecorderManager {
     }
 
     private var recorder: MediaRecorder? = null
+    private var filename: String = ""
 
-    fun start() {
-        TODO("Implement RecorderManager.start()")
+    fun setup(filename: String) {
+        this.filename = filename
+    }
+
+    fun start(context: Context) {
+        recorder?.apply {
+            stop()
+            release()
+        }
+
+        recorder =
+            if (Build.VERSION.SDK_INT >= MEDIA_RECORDER_CONSTRUCTOR_DEPRECATION_API_LEVEL) {
+                MediaRecorder(context)
+            } else {
+                MediaRecorder()
+            }
+
+        recorder!!.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setOutputFile(filename)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("whisper-input", "prepare() failed")
+            }
+
+            start()
+        }
     }
 
     fun stop() {
-        TODO("Implement RecorderManager.stop()")
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
     }
 
 }
