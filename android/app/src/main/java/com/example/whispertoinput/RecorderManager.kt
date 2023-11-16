c8592ee3708337c4f999c5f5a86fe0d9c9ce6567
tigerpaws <boshow2270@gmail.com>
2023-11-16 16:39:48 +0800

    feat(.RecorderManager): Added interface for RecorderManager.
    This class is responsible for encapsulating the process of starting and stopping a MediaRecorder.

---
+++ android/app/src/main/java/com/example/whispertoinput/RecorderManager.kt
@@ -0,0 +1,15 @@
+package com.example.whispertoinput
+
+import android.media.MediaRecorder
+
+class RecorderManager {
+    private var recorder: MediaRecorder? = null
+
+    fun start() {
+        TODO("Implement RecorderManager.start()")
+    }
+
+    fun stop() {
+        TODO("Implement RecorderManager.stop()")
+    }
+}
