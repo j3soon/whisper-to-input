1b508e60e6d59aac9cf1f7a49f6afd1283ec7984
tigerpaws <boshow2270@gmail.com>
2023-12-19 16:07:44 +0800

    feat(): Reads label and desc from settings.xml.

--- android/app/src/main/java/com/example/whispertoinput/settings/SettingsPageBuilder.kt
+++ android/app/src/main/java/com/example/whispertoinput/settings/SettingsPageBuilder.kt
@@ -29,18 +29,50 @@ class SettingsPageBuilder {
 
             // Parse tag starts
             if (event == XmlResourceParser.START_TAG && tag == "setting" && !tagType.isNullOrEmpty()) {
-                val settingEntry: View = when (tagType) {
-                    "text" -> layoutInflater.inflate(R.layout.settings_text, settingsList, false)
-                    "dropdown" -> layoutInflater.inflate(R.layout.settings_dropdown, settingsList, false)
+                event = when (tagType) {
+                    "text" -> buildText(context, parser, layoutInflater, settingsList)
+                    "dropdown" -> buildDropdown(context, parser, layoutInflater, settingsList)
                     else -> { throw Exception("Unknown tag type") }
                 }
-                settingEntry.findViewById<TextView>(R.id.label).text = "Configuration Label"
-                settingEntry.findViewById<TextView>(R.id.description).text = "Descriptive description."
-                settingsList.addView(settingEntry)
-                event = parser.next()
             } else {
                 event = parser.next()
             }
+        }
+    }
+
+    // Inflate and set up a text setting field.
+    private fun buildText(context: Context, parser: XmlResourceParser, layoutInflater: LayoutInflater, settingsList: LinearLayout): Int {
+        val textSetting: View = layoutInflater.inflate(R.layout.settings_text, settingsList, false)
+        textSetting.findViewById<TextView>(R.id.label).text = attrToString(parser, "label", context)
+        textSetting.findViewById<TextView>(R.id.description).text = attrToString(parser, "desc", context)
+        settingsList.addView(textSetting)
+
+        return parser.next()
+    }
+
+    // Inflate and set up a dropdown setting field.
+    private fun buildDropdown(context: Context, parser: XmlResourceParser, layoutInflater: LayoutInflater, settingsList: LinearLayout): Int {
+        val dropdownSetting: View = layoutInflater.inflate(R.layout.settings_dropdown, settingsList, false)
+        dropdownSetting.findViewById<TextView>(R.id.label).text = attrToString(parser, "label", context)
+        dropdownSetting.findViewById<TextView>(R.id.description).text = attrToString(parser, "desc", context)
+        settingsList.addView(dropdownSetting)
+
+        return parser.next()
+    }
+
+    // Supports both string literal and string resource in attributes
+    private fun attrToString(parser: XmlResourceParser, attribute: String, context: Context) : String {
+        val emptyString: String = context.getString(R.string.empty_string)
+        val attributeResource: String = context.getString(parser.getAttributeResourceValue(null, attribute, R.string.empty_string))
+
+        // attributeResource is obtained via context.getString
+        // It is either the actual (parsed) string resource, or an empty string
+        // (when attribute does not exist, or when attribute is a string literal)
+        // If it is an empty string, we instead return the string literal.
+        return if (attributeResource == emptyString) {
+            parser.getAttributeValue(null, attribute)?: ""
+        } else {
+            attributeResource
         }
     }
 }
