bb2fed8773513b6b33dc2561bd7e4e1cec88a901
Tigerpaws <boshow2270@gmail.com>
2024-07-26 00:30:41 +0800

    feat(): updated version from 0.1 to 0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
